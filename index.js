34b9806056165c63f12f8b4b9ccec73460d8f90a
Bhushan-9665 <er.bhushansonawane@gmail.com>
2023-08-02 17:58:50 +0530

    Upadation code 1.2

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-// dustbin--------------------------------------
-
-// popup = classList.add(".open-popup")
-//     console.log()
-
-
-
-// dustbin--------------------------------------
 
 var popup = document.getElementById("popUp")
 const cardContainer = document.getElementById("cardContainer")
 const cardName = document.getElementById('inputTask') // Card Name variable creaded
 const ClosePopupTask = document.getElementById('closePopup1')
-const addTaskFormPopupButton = document.getElementById('addPopup1')
-const taskList = document.getElementById('InputTODO')
+var addTaskFormPopupButton = document.getElementById('addPopup1')
+var taskList = document.getElementById('InputTODO')
+var hideMsg = document.getElementById('hide-msgText')
 
 
 function masterButton(){
@@ -32,21 +25,22 @@ function addcard(){
     // create a temparary Element
     var newcard = document.createElement("div")
     var cardTitle = document.createElement('h2')
-    var ItemList = document.createElement('div')
-    var task = document.createElement('h3')
+    var innerDiv = document.createElement('div')
     var deletButton = document.createElement('button')
     var addButton = document.createElement('button')
+    
     // Append a Element as per reured position
-    newcard.appendChild(cardTitle)
-    newcard.appendChild(ItemList)
-    newcard.appendChild(deletButton)
-    newcard.appendChild(ItemList)
-    newcard.appendChild(task) //--------------
-    newcard.appendChild(addButton)
     cardContainer.appendChild(newcard)
+    newcard.appendChild(cardTitle)
+    newcard.appendChild(innerDiv)
+    newcard.appendChild(deletButton)
+    newcard.appendChild(addButton)
 
     // Add a style by using add class method
     newcard.classList.add('card')
+    deletButton.classList.add('deleteName')
+    addButton.classList.add('addTaskName')
+    
 
 
     // visible button in name in card
@@ -57,30 +51,59 @@ function addcard(){
 
 
 
+    hideMsg.style.display = "none" // hide msg after clinking Add button 
+    closePopup(); // Affer the add card, card will hidden
+
+
+    // Add task in Popup card Using Add button -------------------------------------------
+    addTaskFormPopupButton.addEventListener('click', function(){
+
+        // Child div constracution
+        var innerDiv1 = document.createElement('div')
+        innerDiv.appendChild(innerDiv1)
+
+        innerDiv1.classList.add('childCard')
+
+        
+        
+
+        // inner div containe
+        const taskElement = document.createElement('h3');
+        const delete1 = document.createElement('button')
+        const brTag = document.createElement('hr')
+        
+
+        delete1.innerText = 'Mark Done'
+        taskElement.innerText = taskList.value
+        
+
+        innerDiv1.appendChild(taskElement);
+        innerDiv1.appendChild(delete1)
+        innerDiv1.appendChild(brTag)
+        
+        
+        
+        const taskPopup = document.getElementById('popupWindow-task')
+        taskPopup.style.display = "none"
+})
 
     // Add Task  POPUP visible
-    addButton.addEventListener('click', function addtask(){
+    addButton.addEventListener('click', function(){
         const taskPopup = document.getElementById('popupWindow-task')
         taskPopup.style.display = "flex"
     })
 
 
     // Add Task  POPUP Hidden
-    ClosePopupTask.addEventListener('click', function addtask(){
+    ClosePopupTask.addEventListener('click', function(){
         const taskPopup = document.getElementById('popupWindow-task')
         taskPopup.style.display = "none"
     })
 
-    // Add task in Popup card Using Add button
-    addTaskFormPopupButton.addEventListener('click', function addtask(){
-        task.innerText = taskList.value
-        ItemList.appendChild(task.innerText)
-        
-        const taskPopup = document.getElementById('popupWindow-task')
-        taskPopup.style.display = "none"
-    })
+    
 
-    closePopup(); // Affer the add card, card will hidden
+
+    
 
 
 
@@ -89,4 +112,5 @@ function addcard(){
 
 
 }
+
 
